1558c006f8ac4f3412f5e8e378c891cf4fcfee98
wxgaly <nova_wangxuguang@126.com>
2018-07-06 01:10:41 +0800

    1. 上传文件，修改作者

--- src/main/java/wxgaly/springboot/vue/utils/MyMapper.java
+++ src/main/java/wxgaly/springboot/vue/utils/MyMapper.java
@@ -38,8 +38,9 @@ import tk.mybatis.mapper.common.MySqlMapper;
 /**
  * 继承自己的MyMapper
  *
- * @author liuzh
- * @since 2015-09-06 21:53
+ * @Author WXGALY
+ * @Date 2018-05-22 0:43
+ * @Version 1.0
  */
 public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
     //TODO
